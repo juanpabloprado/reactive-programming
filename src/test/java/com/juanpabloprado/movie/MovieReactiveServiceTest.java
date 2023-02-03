036b1181b9b10e793fa6ca45bb2b9e72e12a606f
Juan Prado <juan@juanpabloprado.com>
2023-02-02 21:25:08 -0600

    make blocking calls

--- src/test/java/com/juanpabloprado/movie/MovieReactiveServiceTest.java
+++ src/test/java/com/juanpabloprado/movie/MovieReactiveServiceTest.java
@@ -1,6 +1,7 @@
 package com.juanpabloprado.movie;
 
 import com.juanpabloprado.domain.Movie;
+import com.juanpabloprado.revenue.RevenueService;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
@@ -8,9 +9,12 @@ import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @MicronautTest
 class MovieReactiveServiceTest {
+    @Inject
+    private RevenueService revenueService;
     @Inject
     private MovieReactiveService movieReactiveService;
 
@@ -57,5 +61,19 @@ class MovieReactiveServiceTest {
                     assertEquals(movieInfo.getReviewList().size(), 2);
                 })
                 .verifyComplete();
+    }
+
+    @Test
+    void getMovieByIdWithRevenue() {
+        long movieId = 100L;
+
+        var movieMono = movieReactiveService.getMovieByIdWithRevenue(movieId).log();
+
+        StepVerifier.create(movieMono)
+              .assertNext(movie -> {
+                  assertEquals("Batman Begins", movie.getMovieInfo().getName());
+                  assertEquals(movie.getReviewList().size(), 2);
+                  assertNotNull(movie.getRevenue());
+              }).verifyComplete();
     }
 }
